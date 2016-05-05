7b6f9e700277e84bb7ff4f6715e678e28f9440ee
cknuesel <c_k_@bluewin.ch>
2016-05-06 01:17:10 +0200

    add my part

--- pic/6_1_grundfrequenztaktung/6_1_2_einst_wirk_und_blindleistung/udc_6_1_2_obsolete.m
+++ pic/6_1_grundfrequenztaktung/6_1_2_einst_wirk_und_blindleistung/udc_6_1_2_obsolete.m
@@ -6,24 +6,24 @@ i = [3.36 2.72 2.16 1.77 1.68 1.8 2.3 2.84];
 p = [-370 -338 -300 -262 -234 -230 -178 -157];
 
 s = u.*i;
-q = sqrt(s-abs(p));
+q = sqrt(s.^2-p.^2);
 
 
 u1 = [293 277 259 241 225 210 195 180];
 i1 = [2.8 2.18 1.55 1.14 1.17 1.6 2.1 2.8];
 p1 = [-368 -334 -305 -273 -244 -206 -185 -160];
 s1 = u1.*i1;
-q1 = sqrt(s1-abs(p1));
+q1 = sqrt(s1.^2-p1.^2);
 
 figure(1)
-plot(u_dc,p,u_dc,p1);
+plot(u_dc,p,u_dc,p1,'LineWidth',5);
 legend('P','P1');
 title('Wirkleistung in Abhängigkeit von Udc');
 xlabel('Gleichspannung [V]');
 ylabel('Wirkleistung [W]');
 
 figure(2)
-plot(u_dc,q, u_dc,q1);
+plot(u_dc,q, u_dc,q1,'LineWidth',5);
 legend('Q','Q1');
 title('Scheinleistung in Abhängigkeit von Udc');
 xlabel('Gleichspannung [V]');
